f159ccc9abbad5f270d48eb85251bda747db4348
darkessence87 <darkessence87@gmail.com>
2023-12-13 23:23:16 +0100

    cmake re-structure

---
+++ dependencies.cmake
@@ -0,0 +1,10 @@
+
+function (find_submodule name path)
+    if (EXISTS ../../${name})
+        set (${path} ../../${name} PARENT_SCOPE)
+    elseif (EXISTS ${3rdPARTY_DIR}/${name})
+        set (${path} ${3rdPARTY_DIR}/${name} PARENT_SCOPE)
+    endif()
+endfunction()
+
+add_subdirectory(3rdparty/psi-tools)
